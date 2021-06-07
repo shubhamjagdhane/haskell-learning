8ca9e031037dd44145a8f9c7aa7bd7f0f5ce9eea
shubhamjagdhane <shubhamjagdhane1010@gmail.com>
2021-06-07 19:56:23 +0530

    updated with error message

--- Personal_Assignments/fact.hs
+++ Personal_Assignments/fact.hs
@@ -1,15 +1,15 @@
 module SafeFact where
 
-data MaybeInt = Success Integer | Stopped String
+data MaybeInt = Success Integer | Stopped
 instance Show MaybeInt where
   show (Success x) = show x
-  show (Stopped y) = show y
+  show Stopped  = "Please pass a positive number!"
 
 fact :: Integer -> MaybeInt
 fact 0 = Success 1
 fact x = case x > 0 of
          True -> go x 1
-         False -> Stopped "Please pass positive number!"
+         False -> Stopped 
 
 go :: Integer -> Integer -> MaybeInt
 go 1 n = Success n
