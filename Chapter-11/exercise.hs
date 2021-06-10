6314e8fe2334e57bae8959f49151e579d39c4300
shubhamjagdhane <shubhamjagdhane1010@gmail.com>
2021-06-10 14:44:00 +0530

    Phone and Hutton’s Razor exercise is remaining

--- Chapter-11/exercise.hs
+++ Chapter-11/exercise.hs
@@ -61,3 +61,37 @@ instance TooMany (Int, Int) where
 instance TooMany (Num a, TooMany a) => (a, a) where
   tooMany x = sum x > 42
 -}
+
+
+-- programmer
+
+data OperatingSystem =
+    GnuPlusLinux
+  | OpenBSDPlusNevermindJustBSDStill
+  | Mac
+  | Windows
+  deriving (Eq, Show)
+
+data ProgrammingLanguage =
+    Haskell
+  | Agda
+  | Idris
+  | PureScript
+  deriving (Eq, Show)
+
+data Programmer =
+  Programmer { 
+    os   :: OperatingSystem,
+    lang :: ProgrammingLanguage 
+  }
+  deriving (Eq, Show)
+
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems = [GnuPlusLinux, OpenBSDPlusNevermindJustBSDStill, Mac, Windows]
+
+allLanguages :: [ProgrammingLanguage]
+allLanguages = [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [Programmer]
+allProgrammers = [Programmer x  y | x <- allOperatingSystems, y <- allLanguages]
--- Chapter-11/learned.hs
+++ Chapter-11/learned.hs
@@ -65,3 +65,75 @@ data Person = Person { name :: String
                      , age  :: Int }
                      deriving (Eq, Show)
 
+-- function type is exponential
+
+-- according to equality of a -> b there should be (total possible valus of type b) ^ (total possible values of type a ) implementation a function a -> b
+
+
+-- binary tree
+
+data BinaryTree a = 
+    Leaf
+  | Node (BinaryTree a) a (BinaryTree a)
+  deriving (Show, Eq)
+
+insert' :: (Ord a) => a -> BinaryTree a -> BinaryTree a
+insert' newData Leaf = Node Leaf newData Leaf
+insert' newData (Node left a right)
+  | newData == a = Node Leaf newData Leaf
+  | newData < a  = Node (insert' newData left) a right
+  | newData > a  = Node left a (insert' newData right)  
+
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree _ Leaf = Leaf
+mapTree f (Node left a right) = 
+  Node (mapTree f left) (f a) (mapTree f right)
+
+
+testTree' :: BinaryTree Integer
+testTree' =
+  Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
+
+mapExpected =
+  Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+
+
+mapOkay =
+  if mapTree (+1) testTree' == mapExpected
+  then print "yup okay!"
+  else error "test failed!"
+
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) = [a] ++ preorder left ++ preorder right
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) = inorder left ++ [a] ++ inorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = postorder left ++ postorder right ++ [a]
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder =
+  if preorder testTree == [2, 1, 3]
+  then putStrLn "Preorder fine!"
+  else putStrLn "Bad news bears."
+
+testInorder :: IO ()
+testInorder =
+  if inorder testTree == [1, 2, 3]
+  then putStrLn "Inorder fine!"
+  else putStrLn "Bad news bears."
+
+testPostorder :: IO ()
+testPostorder =
+  if postorder testTree == [1, 3, 2]
+  then putStrLn "Postorder fine!"
+  else putStrLn "postorder failed check"
