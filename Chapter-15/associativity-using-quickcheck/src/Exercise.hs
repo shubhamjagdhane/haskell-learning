e9e8dec2a713c19e141a47c3d68a85a17f6f8a5b
shubhamjagdhane <shubhamjagdhane1010@gmail.com>
2021-06-16 17:12:20 +0530

    Monoid instance issue for all code in Chapter-15

--- Chapter-15/associativity-using-quickcheck/src/Exercise.hs
+++ Chapter-15/associativity-using-quickcheck/src/Exercise.hs
@@ -7,12 +7,8 @@ newtype First' a = First' { getFirst' :: Optional a } deriving (Eq, Show)
 
 instance Monoid (First' a) where
   mempty = First' Nada
+  mappend (First' x) (First' (Only y)) = First' x
 
-  mappend (First' (Only x)) (First' (Only y)) = First' (Only x)
-
-  mappend (First' (Only x)) Nada = First' (Only x)
-
-  mappend (First' Nada) (First' (Only x))_ = First' (Only x) 
 
 firstMappend :: First' a -> First' a -> First' a
-firstMappend (First' x) (First' y)  = First' $ mappend (First' x) (First' y)
+firstMappend (First' x) (First' y)  = First' x
