f856a65cbb6782b34cd8eb12533079f7bb62a946
shubhamjagdhane <shubhamjagdhane1010@gmail.com>
2021-06-17 00:13:47 +0530

    working successfully, tested with QuickCheck

--- Chapter-15/associativity-using-quickcheck/src/Exercise.hs
+++ Chapter-15/associativity-using-quickcheck/src/Exercise.hs
@@ -1,14 +1,32 @@
 module Exercise where
 import Data.Monoid
+import Test.QuickCheck
+import Test.QuickCheck.Gen (oneof)
 
 data Optional a = Nada | Only a deriving (Eq, Show)
 
 newtype First' a = First' { getFirst' :: Optional a } deriving (Eq, Show)
 
-instance Monoid (First' a) where
-  mempty = First' Nada
-  mappend (First' x) (First' (Only y)) = First' x
+firstGenEqual :: (Arbitrary a) => Gen (First' a)
+firstGenEqual = do
+  a <- arbitrary
+  oneof [return $ First' (Only a),  return $ First' Nada]
+
+instance Arbitrary a => Arbitrary (First' a) where 
+  arbitrary = firstGenEqual
+
+instance Semigroup a => Semigroup (First' a) where
+  (<>) (First' (Only a)) _             = First' {getFirst' = Only a}
+  (<>) (First' Nada) (First' (Only a)) = First' {getFirst' = Only a}
+  (<>) (First' Nada) (First' Nada)     = First' {getFirst' = Nada}
+
+instance Monoid a => Monoid (First' a) where
+  mempty = First' {getFirst' = Nada}
+  mappend (First' (Only x)) (First' (Only y)) = First' {getFirst' = Only x}
+  mappend (First' Nada) (First' (Only x))     = First' {getFirst' = Only x}
+  mappend (First' (Only x)) (First' Nada)     = First' {getFirst' = Only x}
+  mappend (First' Nada) (First' Nada)         = First' {getFirst' = Nada}
 
 
 firstMappend :: First' a -> First' a -> First' a
-firstMappend (First' x) (First' y)  = First' x
+firstMappend (First' (Only x)) (First' (Only y))  = First' {getFirst' = Only x}
--- Chapter-15/associativity-using-quickcheck/src/Main.hs
+++ Chapter-15/associativity-using-quickcheck/src/Main.hs
@@ -8,7 +8,11 @@ import Test.QuickCheck
 import Data.Monoid
 
 
+{-
 data Bull = Fools| Twoo deriving (Eq, Show)
+
+instance Semigroup Bull where
+  (<>) Fools Twoo = Fools
 
 instance Arbitrary Bull where 
   arbitrary = frequency [ (1, return Fools), (1, return Twoo) ]
@@ -17,14 +21,18 @@ instance Monoid Bull where
   mempty      = Fools
   mappend _ _ = Fools
 
+
 type BullMappend = Bull -> Bull -> Bull -> Bool
+-}
+
 type FirstMappend = First' String -> First' String -> First' String -> Bool
 
 main :: IO ()
-main = do
-  quickCheck (monoidAssoc :: BullMappend)
-  quickCheck (monoidLeftIdentity :: Bull -> Bool)
-  quickCheck (monoidRightIdentity :: Bull -> Bool)
+main = do  
+  -- this is suppose to fail
+  -- quickCheck (monoidAssoc :: BullMappend)
+  -- quickCheck (monoidLeftIdentity :: Bull -> Bool)
+  -- quickCheck (monoidRightIdentity :: Bull -> Bool)
 
   quickCheck (monoidAssoc :: FirstMappend)
   quickCheck (monoidLeftIdentity :: First' String -> Bool)
