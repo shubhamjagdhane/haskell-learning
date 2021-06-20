5ebd6014fb6700d2fbe3f6e7c97af19c36e41445
shubhamjagdhane <shubhamjagdhane1010@gmail.com>
2021-06-20 23:31:45 +0530

    added semigroup instance

--- Chapter-15/learned.hs
+++ Chapter-15/learned.hs
@@ -3,7 +3,9 @@ module Listy where
 import Data.Monoid
 
 newtype Listy a = Listy [a] deriving (Eq, Show)
+instance Semigroup a => Semigroup (Listy a) where
+  (<>) (Listy l) (Listy l') = Listy $ mappend l l'
 
-instance Monoid (Listy a) where
+instance Monoid a => Monoid (Listy a) where
   mempty = Listy []
   mappend (Listy l) (Listy l') =  Listy $ mappend l l'
